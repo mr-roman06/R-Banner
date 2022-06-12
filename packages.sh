9478c2a935c3bdd498007b1890be6e391ba03e7d
ROMAN POWER <106998086+romanpower@users.noreply.github.com>
2022-06-12 13:00:44 +0530

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,24 @@
+apt-get update -y
+echo " "
+apt-get upgrade -y
+echo " "
+pkg install figlet -y
+echo " "
+pkg install toilet -y
+echo " "
+pkg install cowsay -y
+echo " "
+pkg install nano -y
+echo " "
+pkg install ruby -y
+echo " "
+gem install lolcat
+echo " "
+pkg install nano -y
+echo ""
+pkg install mpv -y
+echo " "
+pkg install cmatrix
+echo " "
+pkg install python3
+echo " "
---
+++ rmve.sh
@@ -0,0 +1,42 @@
+#!Script  author N117R0
+#By Roman Power
+
+###################################
+cyan='\e[0;36m'
+
+lightgreen='\e[1;32m'
+
+red='\e[1;31m'
+
+yellow='\e[1;33m'
+###################################
+
+clear
+echo -e " \e[91m                 ╔════════════════════════════════════════════════════════════════════════╗ "
+echo -e " \e[91m                 ║   \e[33m██████\e[0m╗        \e[33m██████\e[0m╗  \e[33m█████\e[0m╗ \e[33m███\e[0m╗   \e[33m██\e[0m╗\e[33m███\e[0m╗   \e[33m██\e[0m╗\e[33m███████\e[0m╗\e[33m██████\e[0m╗  \e[91m ║ "
+echo -e " \e[91m                 ║   \e[33m██\e[0m╔══\e[33m██\e[0m╗       \e[33m██\e[0m╔══\e[33m██\e[0m╗\e[33m██\e[0m╔══\e[33m██\e[0m╗\e[33m████\e[0m╗  \e[33m██\e[0m║\e[33m████\e[0m╗  \e[33m██\e[0m║\e[33m██\e[0m╔════╝\e[33m██\e[0m╔══\e[33m██\e[0m╗ \e[91m ║ "
+echo -e " \e[91m                 ║   \e[33m██████\e[0m\e[0m╔╝\e[33m█████\e[0m╗ \e[33m██████\e[0m╔╝\e[33m███████\e[0m║\e[33m██\e[0m╔\e[33m██\e[0m╗ \e[33m██\e[0m║\e[33m██\e[0m╔\e[33m██\e[0m╗ \e[33m██\e[0m║\e[33m█████\e[0m╗  \e[33m██████\e[0m╔╝ \e[91m ║ "
+echo -e " \e[91m                 ║   \e[33m██\e[0m╔══\e[33m██\e[0m╗\e[0m╚════╝ \e[33m██\e[0m╔══\e[33m██\e[0m╗\e[33m██\e[0m╔══\e[33m██\e[0m║\e[33m██\e[0m║╚\e[33m██\e[0m╗\e[33m██\e[0m║\e[33m██\e[0m║╚\e[33m██\e[0m╗\e[33m██\e[0m║\e[33m██\e[0m╔══╝  \e[33m██\e[0m╔══\e[33m██\e[0m╗ \e[91m ║ "
+echo -e " \e[91m                 ║   \e[33m██\e[0m║  \e[33m██\e[0m║       \e[33m██████\e[0m╔╝\e[33m██\e[0m║  \e[33m██\e[0m║\e[33m██\e[0m║ ╚\e[33m████\e[0m║\e[33m██\e[0m║ ╚\e[33m████\e[0m║\e[33m███████\e[0m╗\e[33m██\e[0m║  \e[33m██\e[0m║ \e[91m ║ "
+echo -e " \e[91m                 ║   \e[33m\e[0m╚═\e[0m╝  ╚═╝       \e[0m╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ \e[91m ║\e[96mv.𝟏𝟔 "
+echo -e " \e[91m                 ╚════════════════════════════════════════════════════════════════════════╝  "                                                                                        
+ echo "                                ═════════════ 🇧‌ 🇾‌  🇷‌ 🇴‌ 🇲‌ 🇦‌ 🇳‌  🇪‌ 🇭‌ 🇷‌ ═════════════ "| lolcat       
+ echo ""                                                             
+ echo -e " \e[20m ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴇᴛʜɪᴄᴀʟ ʜᴀᴄᴋɪɴɢ " | lolcat 
+echo " "
+echo ""
+echo "                           rverting wait for 10s please"
+cd $HOME
+cd R-Banner
+cd revert 
+mv bash.bashrc $HOME 
+cd  /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+cd $HOME
+
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+echo "             "EXIT FROM TERMUX AND RE OPEN IT AFTER 5 SECONDS |lolcat 
+echo " "
+echo  "            reverted successfully now you can use.....termux normally"
+sleep 3.0
+cd $HOME
